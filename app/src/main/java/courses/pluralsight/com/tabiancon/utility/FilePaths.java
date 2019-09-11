a94e8077cd340f99f94c45af2537c2da1c53a84a
Losikany <52301343+Losikany@users.noreply.github.com>
2019-09-11 10:20:02 -0700

    Initial commit

---
+++ app/src/main/java/courses/pluralsight/com/tabiancon/utility/FilePaths.java
@@ -0,0 +1,11 @@
+package courses.pluralsight.com.tabiancon.utility;
+
+
+
+public class FilePaths {
+
+    public FilePaths() {
+    }
+
+    public String FIREBASE_IMAGE_STORAGE = "images/users";
+}
